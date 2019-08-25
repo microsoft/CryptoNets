9c0c724b5c2d12ee4dbda068917af7fe0b1c9f64
Ran Gilad-Bachrach <rang@microsoft.com>
2019-08-25 19:40:38 +0000

    CIFAR tested at 740 seconds with accuracy 76.31%

--- CifarCryptoNet/LolaCifarCryptoNet.cs
+++ CifarCryptoNet/LolaCifarCryptoNet.cs
@@ -4,24 +4,40 @@
 ﻿using System;
 using NeuralNetworks;
 using HEWrapper;
+using CommandLine;
 
 
 namespace CifarCryptoNet
 {
+    public class Options
+    {
+        [Option('v', "verbose", Default = false, Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+        [Option('e', "encrypt", Default = false, Required = false, HelpText = "Use encryption")]
+        public bool Encrypt { get; set; }
+    }
     public static class LolaCifarCryptoNet
     {
         public static void Main(string[] args)
         {
+            var options = new Options();
+            var parsed = Parser.Default.ParseArguments<Options>(args).WithParsed(x => options = x);
+            if (parsed.Tag == ParserResultType.NotParsed) Environment.Exit(-2);
+
             WeightsReader wr = new WeightsReader("CifarWeight.csv", "CifarBias.csv");
-            // current network has has the potential of 76.76% accuracy
-            // with current parameters its accuracy is 75.01% and uses 68.25 bits
+            // Model has accuracy of 76.5
+            // Current parameters (scale) provide accuracy of 76.31% and uses 78.55 + 1 bits in message length
+            // It has a latency of 740 seconds on the reference machine (Azure B8ms server at rest)
 
             Console.WriteLine("Generating encryption keys {0}", DateTime.Now);
-            var factory = new EncryptedSealBfvFactory(new ulong[] { 8590163969, 8590458881 }, 16384, DecompositionBitCount: 60, GaloisDecompositionBitCount: 60, SmallModulusCount: 7);
-            //var factory = new RawFactory(16 * 1024);
+            IFactory factory = null;
+            if (options.Encrypt)
+                factory = new EncryptedSealBfvFactory(new ulong[] { 957181001729, 957181034497 }, 16384, DecompositionBitCount: 60, GaloisDecompositionBitCount: 60, SmallModulusCount: 8);
+            else
+                factory = new RawFactory(16 * 1024);
             Console.WriteLine("Encryption keys ready {0}", DateTime.Now);
-            int numberOfRecords = 10;
-            bool verbose = false;
+            int numberOfRecords = 10000;
+            bool verbose = options.Verbose;
 
             string fileName = "cifar-test.tsv";
             var readerLayer = new LLConvReader
@@ -34,7 +50,7 @@ namespace CifarCryptoNet
                 Lowerpadding = new int[] { 0, 1, 1 },
                 Stride = new int[] { 1000, 2, 2 },
                 NormalizationFactor = 1.0 / 256.0,
-                Scale = 8.0,
+                Scale = 8,
                 Verbose = verbose
             };
 
@@ -52,7 +68,7 @@ namespace CifarCryptoNet
                 Lowerpadding = new int[] { 0, 1, 1 },
                 Stride = new int[] { 1000, 2, 2 },
                 MapCount = new int[] { 83, 1, 1 },
-                WeightsScale = 100.0,
+                WeightsScale = 256.0,
                 Weights = (double[])wr.Weights[0],
                 Bias = (double[])wr.Biases[0],
                 Verbose = verbose
@@ -79,13 +95,13 @@ namespace CifarCryptoNet
                 Upperpadding = new int[] { 0, 4, 4 },
                 Lowerpadding = new int[] { 0, 4, 4 },
                 Stride = new int[] { 83, 2, 2 },
-                MapCount = new int[] { 130, 1, 1 }
+                MapCount = new int[] { 112, 1, 1 }
             };
 
             var DenseLayer4 = new LLDenseLayer
             {
                 Source = ActivationLayer3,
-                WeightsScale = 128.0,
+                WeightsScale = 512.0,
                 Weights = ConvEngine.GetDenseWeights((double[])wr.Weights[1]),
                 Bias = ConvEngine.GetDenseBias((double[])wr.Biases[1]),
                 InputFormat = EVectorFormat.dense,
@@ -105,13 +121,12 @@ namespace CifarCryptoNet
                 Source = ActivationLayer5,
                 Weights = (double[])wr.Weights[2],
                 Bias = (double[])wr.Biases[2],
-                WeightsScale = 256.0,
+                WeightsScale = 512.0,
                 InputFormat = EVectorFormat.dense,
                 Verbose = verbose
             };
 
             var StopTimingLayer = new TimingLayer() { Source = DenseLayer6, StopCounters = new string[] { "Inference-Time" } };
-
 
             var network = StopTimingLayer;
             Console.WriteLine("Preparing");
@@ -122,6 +137,7 @@ namespace CifarCryptoNet
             while (count < numberOfRecords)
             {
                 using (var m = network.GetNext())
+                {
                     Utils.ProcessInEnv(env =>
                     {
                         var decrypted = m.Decrypt(env);
@@ -133,13 +149,22 @@ namespace CifarCryptoNet
                         if (pred != readerLayer.Labels[0]) errs++;
                         count++;
                         if (count % batchSize == 0)
-                            Console.WriteLine("errs {0}/{1} accuracy {2:0.000}% prediction {3} label {4} {5}", errs, count, 100 - (100.0 * errs / (count)), pred, readerLayer.Labels[0], TimingLayer.GetStats());
+                        {
+                            Console.Write("errs {0}/{1} accuracy {2:0.000}% prediction {3} label {4} {5}ms", errs, count, 100 - (100.0 * errs / (count)), pred, readerLayer.Labels[0], TimingLayer.GetStats());
+                            if (options.Encrypt)
+                                Console.WriteLine();
+                            else
+                                Console.WriteLine(" {0}bits", Math.Log(RawMatrix.Max) / Math.Log(2));
+
+                        }
 
                     }, factory);
+                }
             }
             Console.WriteLine("errs {0}/{1} accuracy {2:0.000}%", errs, count, 100 - (100.0 * errs / (count)));
             network.DisposeNetwork();
-            Console.WriteLine("Max computed value {0} ({1})", RawMatrix.Max, Math.Log(RawMatrix.Max) / Math.Log(2));
+            if (!options.Encrypt)
+                Console.WriteLine("Max computed value {0} ({1})", RawMatrix.Max, Math.Log(RawMatrix.Max) / Math.Log(2));
         }
     }
 }
